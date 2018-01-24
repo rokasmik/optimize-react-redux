7f6cdc6d2310dfc6eb63f7d3e98e97aac0a0ef2b
Rokas Mikalkevičius <rokasm@wix.com>
2018-01-24 23:47:49 +0200

    extend for preview

--- src/components/Item.js
+++ src/components/Item.js
@@ -4,11 +4,11 @@ import css from './Item.css';
 
 const Item = ({ name, count, liked, likeItem, addOne }) => {
 
-  const className = 'item' + (liked ? ' liked' : '');
+  const nameClassName = 'itemName' + (liked ? ' liked' : '');
 
   return (
-    <div className={className}>
-      <div className="itemName">{ name }</div>
+    <div className="item">
+      <div className={nameClassName}>{ name }</div>
       <div className="itemCount">{ count }</div>
       <div className="hideButton" onClick={likeItem}>{'\u2665'}</div>
       <div className="addButton" onClick={addOne}>+</div>
